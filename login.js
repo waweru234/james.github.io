505d4a47f61e5749c8287afe0a16abb60963fb43
waweru234 <129942153+waweru234@users.noreply.github.com>
2025-04-08 08:00:11 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+// Function to sign up a new user
+// Import the necessary Firebase modules from v11
+
+
+  
+  /* 
+  Example usage:
+  For sign up:
+  signUpUser("john@example.com", "password123", "John Doe", "+123456789");
+  
+  For login:
+  loginUser("john@example.com", "password123");
+  */
+  
